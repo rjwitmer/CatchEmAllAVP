148c3705c96dfce40667ec8135a2e619d298e70b
Bob Witmer <bwitmer@golden.net>
2025-08-28 14:07:08 -0400

    Initial Commit

---
+++ CatchEmAllAVP/CatchEmAllAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllAVPApp.swift
+//  CatchEmAllAVP
+//
+//  Created by Bob Witmer on 2025-08-28.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
